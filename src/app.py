71368fbbe80a9834ebfb531c91316ebf7ac119da
coriotocelis <juancelismeza@gmail.com>
2024-12-12 17:17:42 -0500

    Funcion de cargar json

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 import json
 import os
 from flask import Flask, render_template, request, redirect, url_for
+from flask import flash
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 
 Base = declarative_base()
 DATABASE_URL = "sqlite:///tareas.db"
@@ -23,6 +23,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
 @app.route('/')
 def index():
@@ -60,6 +61,44 @@ def guardar():
     tareas_lista = [{"id": tarea.id, "titulo": tarea.titulo, "descripcion": tarea.descripcion, "completada": tarea.completada} for tarea in tareas]
     with open("tareas.json", "w") as file:
         json.dump(tareas_lista, file, indent=4)
+    return redirect(url_for('index'))
+
+@app.route('/cargar', methods=['POST'])
+def cargar():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    
+    if file and file.filename.endswith('.json'):
+        try:
+
+            data = json.load(file)
+
+            session.query(Tarea).delete()
+            session.commit()
+
+            for tarea_data in data:
+                nueva_tarea = Tarea(
+                    id=tarea_data['id'],
+                    titulo=tarea_data['titulo'],
+                    descripcion=tarea_data['descripcion'],
+                    completada=tarea_data['completada']
+                )
+                session.add(nueva_tarea)
+            
+            session.commit()
+            flash('Archivo JSON cargado exitosamente.')
+        except json.JSONDecodeError:
+            flash('Error al leer el archivo JSON.')
+    else:
+        flash('El archivo debe ser un JSON.')
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
